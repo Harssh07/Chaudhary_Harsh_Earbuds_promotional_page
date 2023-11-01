afa859ce62276dd7573380683974a8e30228bddc
Harssh07 <121983623+Harssh07@users.noreply.github.com>
2023-11-01 16:56:27 -0400

    images and js added

---
+++ js/main.js
@@ -0,0 +1,44 @@
+(() => {
+    let imageCon = document.querySelector('#imageCon'),
+        drag = document.querySelector('.image-drag'),
+        left = document.querySelector('.image-left'),
+        dragging = false,
+        min = 0,
+        max = imageCon.offsetWidth;
+
+        function onDown() {
+            dragging = true;
+            console.log("set to true")
+        }
+
+        function onUp() {
+            dragging = false;
+            console.log("set to false")
+        }
+
+        function onMove(event) {
+            // console.log("on move called")
+            if(dragging==true) {
+                // console.log("dragging");
+                let x = event.clientX - imageCon.getBoundingClientRect().left;
+                console.log(x);
+                
+                if(x < min) {
+                    x=min;
+                } else if (x > max) {
+                    x = max-10;
+                }
+
+                drag.style.left = x + "px";
+                left.style.width = x + "px";
+            }
+
+    }
+
+        drag.addEventListener('mousedown', onDown);
+        document.body.addEventListener('mouseup', onUp);
+        document.body.addEventListener('mousemove', onMove);
+    
+
+  
+})();
